4940dd8b3751c1e8a99e6a54907c3537060bf281
Siddharth Vasu <siddharth_vasu@hotmail.co.uk>
2022-08-04 10:06:53 +0100

    handle mono, use vectorized numpy calcs

--- audioSurface.py
+++ audioSurface.py
@@ -1,18 +1,12 @@
 import argparse
-from operator import length_hint
 import numpy as np
 import scipy.io.wavfile as scio
-import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-import wave
 import sys
-import sounddevice as sd
 import pygame
-from mpl_toolkits.mplot3d import axes3d, Axes3D
+from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
-import itertools
-from collections import deque
 
 
 class SongSurface:
@@ -33,45 +27,56 @@ class SongSurface:
 
     def get_buf(self):
         min_size = min(self.count, self.length)
-        x0 = self.x[:min_size].reshape(-1)
-        y0 = self.y[:min_size].reshape(-1)
-        z0 = self.z[:min_size].reshape(-1)
+        if min_size < self.length:
+            x0 = self.x[:min_size].reshape(-1)
+            y0 = self.y[:min_size].reshape(-1)
+            z0 = self.z[:min_size].reshape(-1)
+        else:  # buffer rolled over
+            idx = self.count % self.length
+            x0 = np.append(self.x[idx + 1 : self.length], self.x[: idx + 1])
+            y0 = np.append(self.y[idx + 1 : self.length], self.y[: idx + 1])
+            z0 = np.append(self.z[idx + 1 : self.length], self.z[: idx + 1])
+
         return x0, y0, z0
 
 
 def getFrames(fnum, samples, rate, samp, interval, song_surface, ax, plot):
     offset = int((interval / 1000.0) * rate)
-    sl0 = samples[offset * fnum : offset * fnum + samp, 0]
-    sl0 = np.multiply(np.hamming(len(sl0)), sl0)
-    freq0 = abs(np.fft.fft(sl0))
-    # dsample0 = [np.mean(j) for j in np.split(freq0,samp/1)]
-    r = np.arange(-rate / 2, rate / 2, rate / len(freq0))[0 : len(freq0)]
-    song_surface.append_buf(
-        r,
-        [20 * np.log10(i) for i in freq0],
-        [(interval / 1000.0) * fnum for j in freq0],
+    sl0 = 0.5 * (
+        samples[offset * fnum : offset * fnum + samp, 0]
+        + samples[offset * fnum : offset * fnum + samp, 1]
     )
+    if len(sl0):
+        sl0 = np.multiply(np.hamming(len(sl0)), sl0)
+        freq0 = np.absolute(np.fft.fft(sl0))
+        # dsample0 = [np.mean(j) for j in np.split(freq0,samp/1)]
+        x0 = np.arange(0, rate, rate / len(freq0))[: len(freq0)]
+        t = (interval / 1000.0) * fnum
+        song_surface.append_buf(x0, t * np.ones(len(freq0)), 20 * np.log10(freq0))
+        ax.clear()
+        ax.set_xlabel("Frequency (Hz)")
+        ax.set_ylabel("Time (s)")
+        ax.set_zlim(-10.0, 80, 0)
+        ax.set_zlabel("Volume (dB)")
 
-    ax.clear()
-    ax.set_xlabel("Frequency (Hz)")
-    ax.set_ylabel("Time (s)")
-    ax.set_zlim(-10.0, 80, 0)
-    ax.set_zlabel("Volume (dB)")
-
-    # merge ndarray into lists for plot
-    x0 = song_surface.x.reshape(-1)
-    y0 = song_surface.y.reshape(-1)
-    z0 = song_surface.z.reshape(-1)
-    plot = ax.plot_trisurf(x0, y0, z0, cmap=cm.jet, linewidth=0.2)
+        # merge ndarray into lists for plot
+        xo, yo, zo = song_surface.get_buf()
+        plot = ax.plot_trisurf(xo, yo, zo, cmap=cm.jet, linewidth=0.2)
     return (
         song_surface,
         plot,
     )
 
-def run_plot(file_name, memory, interval):
+
+def plot_animation(file_name: str, play: bool, memory: int, interval: int):
     # Once filename is correctly obtained, obtain samples and sample rate.
     rate, samples = scio.read(file_name)
     song_surface = np.shape(samples)
+    if len(song_surface) == 1:  # audio is mono, make it stereo for now by copying.
+        samples_dbl = np.zeros((song_surface[0], 2))
+        samples_dbl[:, 0] = samples
+        samples_dbl[:, 1] = samples
+        samples = samples_dbl
     print(f"Sample Rate: {rate}Hz, Samples : {song_surface}", file=sys.stderr)
     r = np.arange(0, song_surface[0], rate)
     # samp is the number of samples in the analysis frame. Has to be smaller than rate.
@@ -89,46 +94,51 @@ def run_plot(file_name, memory, interval):
     i = 0
     sl0 = samples[i : i + samp, 0]
     sl0 = np.multiply(np.hamming(len(sl0)), sl0)
-    freq0 = abs(np.fft.fft(sl0))
+    freq0 = np.absolute(np.fft.fft(sl0))
     if bins > 1:
         freq0 = [np.mean(j) for j in np.split(freq0, samp / bins)]
-    x0 = np.arange(-rate / 2, rate / 2, rate / len(freq0))[0 : len(freq0)]
-    z0 = [20 * np.log10(j) for j in freq0]
-    y0 = [count for j in freq0]
-    y1 = [count + 0.01 for j in freq0]
+    x0 = np.arange(0, rate, rate / len(freq0))[: len(freq0)]
+    z0 = 20 * np.log10(freq0)
+    y0 = count * np.ones(freq0.shape)
+    y1 = (count + 0.01) * np.ones(freq0.shape)
     song_surface = SongSurface(memory, samp)
     song_surface.append_buf(x0, y0, z0)
     song_surface.append_buf(x0, y1, z0)
     xo, yo, zo = song_surface.get_buf()
     plot = ax.plot_trisurf(xo, yo, zo, cmap=cm.jet, linewidth=0.2)
-    # pygame.mixer.init()
-    # pygame.mixer.music.load(fname)
-    # pygame.mixer.music.play()
-    plt.rcParams["animation.ffmpeg_path"] = "/usr/bin/ffmpeg"
-    mywriter = animation.FFMpegWriter(fps=30, bitrate=5000)
+    pygame.mixer.init()
+    pygame.mixer.music.load(file_name)
+    pygame.mixer.music.play()
+    mywriter = animation.FFMpegWriter(fps=60, bitrate=5000)
     ani = animation.FuncAnimation(
         fig,
         getFrames,
         fargs=(samples, rate, samp, interval, song_surface, ax, plot),
         interval=interval,
-        save_count=50,
         blit=False,
     )
     plt.show()
-
+    ani.save("video.mp4", writer=mywriter)
 
 
 def main():
     # define key parameters
     memory = 5
-    interval = 60
+    interval = 16
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "file_name", help="file name to convert into a surface plot. Should end in .wav"
     )
+    parser.add_argument(
+        "--play",
+        help="play music as plot draws",
+        action="store_true",
+        default=False,
+        required=False,
+    )
     args = parser.parse_args()
     if args.file_name and args.file_name.endswith(".wav"):
-        run_plot(args.file_name, memory, interval)
+        plot_animation(args.file_name, args.play, memory, interval)
     else:
         print(f"File name: {args.file_name} invalid!", file=sys.stderr)
 
